6f8a3f1118375201b2de5116f46f3b3a54ea079a
Loi Tran <datloiboi@gmail.com>
2023-06-06 16:50:01 -0400

    1.0.47

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import { Counter } from "./src/lib/components/Counter";
+import Counter, { add, LIFE } from "./src/lib/Counter";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Counter life={10} />
+      <Counter life={LIFE} />
     </View>
   );
 }
@@ -16,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     backgroundColor: "#fff",
+    alignItems: "center",
     justifyContent: "center",
   },
 });
