3b19ec30776fe35b87cf43b9755525e223597869
Loi Tran <datloiboi@gmail.com>
2023-06-06 16:46:40 -0400

    1.0.46 - Works with Stylesheet matching expo version

--- Counter.js
+++ Counter.js
@@ -1,16 +1,44 @@
 import React, { useState } from "react";
-import { 
-  View, 
-  Text, 
-  StyleSheet,
-  TouchableOpacity
-} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-function Counter() {
+function Counter({ life = 1 }) {
   const [count, setCount] = useState(0);
   return (
-    <View>
-      <Text>Counter {count}</Text>
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontWeight: 300,
+          fontSize: 30,
+        }}
+      >
+        Counter
+      </Text>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View
+          style={{
+            flex: 1,
+            height: 1,
+            backgroundColor: "black",
+          }}
+        />
+        <View>
+          <Text
+            style={{
+              fontSize: 30,
+              color: "gold",
+            }}
+          >
+            {count * life}
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            height: 1,
+            backgroundColor: "black",
+          }}
+        />
+      </View>
       <TouchableOpacity onPress={() => setCount(count + 1)}>
         <Text style={{ color: "green", fontSize: 20 }}>Increment</Text>
       </TouchableOpacity>
@@ -21,27 +49,20 @@ function Counter() {
   );
 }
 
-export { Counter };
+const LIFE = 42;
 
-const styles = StyleSheet.create({ 
+const add = (n) => n + LIFE;
+
+const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '50%',
-    maxHeight: '50%',
+    width: "100%",
+    height: "50%",
+    maxHeight: "50%",
     justifyContent: "center",
     alignItems: "center",
-  },
-  header: {
-    fontWeight: 300,
-    fontSize: 30
-  },
-  count: {
-    fontSize: 30,
-    color: "gold"
-  },
-  line: { 
-    flex: 1, 
-    height: 1, 
-    backgroundColor: "black" 
   }
 })
+
+export default Counter;
+
+export { Counter, add, LIFE };
